d6f09e593d2e2708550400d2396e4fbc2d6eeb02
FabioLapiana <131192770+FabioLapiana@users.noreply.github.com>
2023-08-01 17:53:00 +0200

    add part 1 files

---
+++ bookmarks/images/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
+
+from .models import Image
+from .forms import ImageUploadForm
+
+@login_required
+def image_upload(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = request.user
+            image.save()
+            return redirect('images:detail', id=image.id, slug=image.slug)
+    else:
+        form = ImageUploadForm()
+    return render(request, 'images/image/image_upload.html', {'form': form})
+
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+
+    liked_images = Image.objects.filter(users_like=request.user)
+
+    if images_only:
+        return render(request, 'images/image/list_images.html', {
+            'section': 'images',
+            'images': images,
+            'liked_images': liked_images,  # Passa le immagini piaciute dall'utente
+        })
+    
+    return render(request, 'images/image/list.html', {
+        'section': 'images',
+        'images': images,
+        'liked_images': liked_images,  # Passa le immagini piaciute dall'utente
+    })
